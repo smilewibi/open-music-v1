a695516afffc9fbd65a91325fadd2a6d44e83182
ismail <wibidjokam@gmail.com>
2021-07-16 10:40:15 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,36 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+
+const init = async () => {
+  const songsService = new SongsService();
+
+  const server = Hapi.server({
+    // port: 5000,
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
