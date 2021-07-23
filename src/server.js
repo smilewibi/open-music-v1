800d6432f8e5952d36d9838fe7997a9a33f7cea4
ismail <wibidjokam@gmail.com>
2021-07-23 07:34:57 +0700

    implemented server error to server js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const Hapi = require('@hapi/hapi');
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
+const ServerError = require('./exceptions/ServerError');
 
 const init = async () => {
   const songsService = new SongsService();
@@ -27,6 +29,32 @@ const init = async () => {
       service: songsService,
       validator: SongsValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+  
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+  
+    // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    if (response instanceof ServerError) {
+      const newResponse = h.response({
+        status: 'error',
+        message: response.message,
+      });
+
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    return response.continue || response;
   });
 
   await server.start();
